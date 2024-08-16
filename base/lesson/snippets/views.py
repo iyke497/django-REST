c6aa927fa27a916613945a3abb51c17448d8bf09
Ikechukwu <iyke497@gmail.com>
2024-08-16 08:54:42 +0100

    Using mixins

--- base/lesson/snippets/views.py
+++ base/lesson/snippets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, mixins, generics
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -98,3 +98,34 @@ class SnippetDetail(APIView):
 		snippet = self.get_object(pk)
 		snippet.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#Using mixins
+class SnippetList(mixins.ListModelMixin,
+				mixins.CreateModelMixin,
+				generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+	def get(self, request, *args, **kwargs):
+		return self.list(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+					mixins.UpdateModelMixin,
+					mixins.DestroyModelMixin,
+					generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+	def get(self, request, *args, **kwargs):
+		return self.retrieve(request, *args, **kwargs)
+
+	def put(self, request, *args, **kwargs):
+		return self.update(request, *args, **kwargs)
+
+	def delete(self, request, *args, **kwargs):
+		return self.destroy(request, *args, **kwargs)
