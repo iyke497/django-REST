0fa32d57c68ff7d971384069b957f10f6707521c
Ikechukwu <iyke497@gmail.com>
2024-08-17 11:38:23 +0100

    Adding endpoints for our user models

--- base/lesson/snippets/serializers.py
+++ base/lesson/snippets/serializers.py
@@ -1,23 +1,19 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
 	"""docstring for SnippetSerializer"""
 	class Meta:
 		model = Snippet
-		fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+		fields = ['id', 'title', 'code', 'linenos', 'language', 'style'] #optimisation, problem aalways need instance of object 
+																		 #n+1 problem, happens with lots of relationships
 
-	def create(self, validated_data):
-		"""
-		Create and return a new `snippet` instance, given the validated data.
-		"""
-		return Snippet.objects.create(**validated_data)
 
-	def update(self, instance, validated_data):
-		"""
-		Update and return an existing `Snippet` instance, given the validated data.
-		"""
-		pass
-		
+class UserSerializer(serializers.ModelSerializer):
+	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+	class Meta:
+		model = User 
+		fields = ['id', 'username', 'snippets']
