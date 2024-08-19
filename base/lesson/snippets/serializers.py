d6c3bcb964bbb1c5baa20d2f762e02d189ccd0a5
Ikechukwu <iyke497@gmail.com>
2024-08-19 13:59:24 +0100

    Associate snippets with users - update our serializer

--- base/lesson/snippets/serializers.py
+++ base/lesson/snippets/serializers.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
 	"""docstring for SnippetSerializer"""
+	owner = serializers.ReadOnlyFeild(source='owner.username')
 	class Meta:
 		model = Snippet
-		fields = ['id', 'title', 'code', 'linenos', 'language', 'style'] #optimisation, problem aalways need instance of object 
+		fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner'] #optimisation, problem aalways need instance of object 
 																		 #n+1 problem, happens with lots of relationships
 
 
