212c0869e2283cb95a3ca42d753ff4a91f066994
Iyke <iyke497@gmail.com>
2024-08-22 10:35:19 +0100

    minor fixes

--- base/lesson/snippets/views.py
+++ base/lesson/snippets/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers, viewsets
